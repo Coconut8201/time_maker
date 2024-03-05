1e38eca34194f8be5646b446d67f6869b04fe15d
Coconut8201 <cocom714kv@gmail.com>
2024-03-05 14:36:49 +0800

    init

---
+++ whole_time.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class time {
+    int hour;
+    int minute;
+
+    @Override
+    public String toString(){
+        String ans = this.hour+ ":" + this.minute;
+        return ans;
+    }
+}
+//學號、姓名、生日、年齡、地址、電話及專長
